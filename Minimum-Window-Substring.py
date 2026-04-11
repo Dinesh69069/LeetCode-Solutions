318876fae4de762d03c624f19ddc508107db6ec0
DINESH KUMAR SAHOO <dineshkumarcs0014@gmail.com>
2026-04-11 19:07:27 +0530

    Update Minimum-Window-Substring.py

--- Minimum-Window-Substring.py
+++ Minimum-Window-Substring.py
@@ -1,31 +1,30 @@
-1class Solution:
-2    def minWindow(self, s: str, t: str) -> str:
-3        target_map = {}
-4        window_map = {}
-5        have , need = 0 , 0
-6        for i in t :
-7            if i in target_map:
-8                target_map[i] +=1
-9            else:
-10                target_map[i] = 1
-11                need+=1
-12        left , min_len ,min_str = 0 , len(s)+1 , ''
-13        for right in range(len(s)):
-14            if s[right] in window_map:
-15                window_map[s[right]] +=1
-16            else:
-17                window_map[s[right]] = 1
-18            if((s[right] in target_map) and (window_map[s[right]] == target_map[s[right]])):
-19                have +=1
-20            while(need==have):
-21                if((right - left + 1) < min_len ):
-22                    min_len = right - left +1
-23                    min_str = s[left:right+1]
-24                window_map[s[left]] -=1
-25                if((s[left] in target_map) and (window_map[s[left]]<target_map[s[left]])):
-26                    have-=1
-27                left+=1
-28        return min_str
-29__import__('atexit').register(lambda: open('display_runtime.txt', 'w').write('000'))
-30
-31
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        target_map = {}
+        window_map = {}
+        have , need = 0 , 0
+        for i in t :
+            if i in target_map:
+                target_map[i] +=1
+            else:
+                target_map[i] = 1
+                need+=1
+        left , min_len ,min_str = 0 , len(s)+1 , ''
+        for right in range(len(s)):
+            if s[right] in window_map:
+                window_map[s[right]] +=1
+            else:
+                window_map[s[right]] = 1
+            if((s[right] in target_map) and (window_map[s[right]] == target_map[s[right]])):
+                have +=1
+            while(need==have):
+                if((right - left + 1) < min_len ):
+                    min_len = right - left +1
+                    min_str = s[left:right+1]
+                window_map[s[left]] -=1
+                if((s[left] in target_map) and (window_map[s[left]]<target_map[s[left]])):
+                    have-=1
+                left+=1
+        return min_str
+
+
