e309bad6987c102d391ff79d68939b878cceca7a
DINESH KUMAR SAHOO <dineshkumarcs0014@gmail.com>
2026-04-21 19:38:31 +0530

    [287] [Time Beats: 54.96%] [Memory Beats: 75.96%] - LeetPush

---
+++ Find-the-Duplicate-Number.py
@@ -0,0 +1,13 @@
+1class Solution:
+2    def findDuplicate(self, nums: List[int]) -> int:
+3        fast = slow = nums[0]
+4        while(fast!=len(nums)):
+5            slow = nums[slow]
+6            fast = nums[nums[fast]]
+7            if(slow ==fast):
+8                fast = nums[0]
+9                while(fast != slow):
+10                    slow = nums[slow]
+11                    fast = nums[fast]
+12                return fast
+13        
